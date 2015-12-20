0dbe06f3f064ec134bddb966645a2eacf1aa4c86
Ernesto Crespo Avila <ecrespo@gmail.com>
2015-12-19 23:02:15 -0430

    Script que consulta los responsables

---
+++ src/bd/pruebadb-responsables.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+"""
+Script de pruebas de base de datos con sqlalchemy
+"""
+import model
+from sqlalchemy import orm
+from sqlalchemy import create_engine
+
+
+
+#Crear un engine y crear todas las tablas necesarias
+
+#Se instancia el engine con la base de datos sms.db deshabilitando el eco.
+engine = create_engine('sqlite:///sms.db', echo=False)
+
+#Se define el metadato y se crea
+model.metadata.bind = engine
+model.metadata.create_all()
+
+#Se crea la session
+sm = orm.sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_commit=True)
+session = orm.scoped_session(sm)
+
+
+#Insertar un responsable:
+responsable = model.Responsables()
+
+responsable.responsable = u"Ernesto Crespo"
+
+responsable.celular = u"04265673018"
+responsable.correo = u"ecrespo@gmail.com"
+print (type(responsable))
+
+session.add(responsable)
+
+session.flush()
+
+session.commit()
+
+
+
+
+
+
+#Se realiza una consulta a la tabla responsables
+
+print ("Consulta inicial de la tabla responsables")
+
+consulta = session.query(model.Responsables).all()
+
+for lista in consulta:
+
+    print (lista.responsable,lista.celular,lista.correo)
+
