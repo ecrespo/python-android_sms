25244212f88b08774da93fca6f49e40e0dfc3454
Ernesto Crespo Avila <ecrespo@gmail.com>
2015-12-23 20:13:07 -0430

    Probada la insercion y consultas de la tabla dispositivos

--- src/bd/pruebadb-dispositivos.py
+++ src/bd/pruebadb-dispositivos.py
@@ -23,16 +23,15 @@ sm = orm.sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_c
 session = orm.scoped_session(sm)
 
 
-#Insertar un responsable:
-responsable = model.Responsables()
+#Insertar dispositivos:
+dispositivos = model.Dispositivos()
 
-responsable.responsable = u"Ernesto Crespo"
+dispositivos.dispositivo = "0403725B09015010" 
+dispositivos.descripcion = u"Celular motorola"
+dispositivos.estatus = True
 
-responsable.celular = u"04265673018"
-responsable.correo = u"ecrespo@gmail.com"
-print (type(responsable))
 
-session.add(responsable)
+session.add(dispositivos)
 
 session.flush()
 
@@ -45,59 +44,13 @@ session.commit()
 
 #Se realiza una consulta a la tabla grupos
 
-print "Consulta inicial de la tabla contactos"
+print "Consulta inicial de la tabla Dispositivos"
 
-consulta = session.query(model.Contactos).all()
-
-for lista in consulta:
-
-    print lista.contacto,lista.numcel
-
-print "--------------------------------"
-
-
-
-
-
-#Agregar un contacto
-
-contacto = model.Contactos()
-
-contacto.contacto = u"Luisa Gonzalez"
-
-contacto.numcel = u"04155555555"
-print (type(contacto))
-
-session.add(contacto)
-
-session.flush()
-
-session.commit()
-
-
-#Se realiza una consulta a la tabla grupos
-
-print "Consulta la tabla contactos con el dato incorporado"
-
-consulta = session.query(model.Contactos).all()
+consulta = session.query(model.Dispositivos).all()
 
 for lista in consulta:
 
-    print lista.contacto,lista.numcel
+    print lista.dispositivo,lista.descripcion,lista.estatus
 
-print "--------------------------------"
-
-
-
-
-
-
-
-#Borrar un contacto
-#session.delete(contacto)
-
-#session.flush()
-
-#session.commit()
 
 
