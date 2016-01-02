b93c910b09252819267f1ddf78b056d19249c520
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-01 20:37:34 -0430

    Script de prueba de bitacora,que facilita insertar datos a la tabla bitacora

--- src/bd/pruebadb-bitacora.py
+++ src/bd/pruebadb-bitacora.py
@@ -2,10 +2,19 @@
 
 """
 Script de pruebas de base de datos con sqlalchemy
+Tabla bitacora
 """
+import datetime
+
 import model
 from sqlalchemy import orm
 from sqlalchemy import create_engine
+
+
+#
+def now():
+    """Funcion que devuelve la hora en el momento que se ejecuta la funcion."""
+    return datetime.datetime.now()
 
 
 
@@ -24,20 +33,55 @@ session = orm.scoped_session(sm)
 
 
 #Insertar un responsable:
-responsable = model.Responsables()
+bitacora1 = model.Bitacora()
+bitacora1.id = 1
+bitacora1.mensaje = 1
+bitacora1.grupo = 1
+bitacora1.numcel = "04265673018"
+bitacora1.contacto = 1
+bitacora1.timestamp = now()
+bitacora1.estatus = True
 
-responsable.responsable = u"Ernesto Crespo"
 
-responsable.celular = u"04265673018"
-responsable.correo = u"ecrespo@gmail.com"
-print (type(responsable))
-
-session.add(responsable)
+session.add(bitacora1)
 
 session.flush()
 
 session.commit()
 
+#Insertar un responsable:
+bitacora2 = model.Bitacora()
+bitacora2.id = 2
+bitacora2.mensaje = 2
+bitacora2.grupo = 2
+bitacora2.numcel = "04275673018"
+bitacora2.contacto = 2
+bitacora2.timestamp = now()
+bitacora2.estatus = False
+
+
+session.add(bitacora2)
+
+session.flush()
+
+session.commit()
+
+#Insertar un responsable:
+bitacora3 = model.Bitacora()
+bitacora3.id = 3
+bitacora3.mensaje = 3
+bitacora3.grupo = 3
+bitacora3.numcel = "04245673018"
+bitacora3.contacto = 3
+bitacora3.timestamp = now()
+bitacora3.estatus = True
+
+
+session.add(bitacora3)
+
+session.flush()
+
+session.commit()
 
 
 
@@ -47,57 +91,16 @@ session.commit()
 
 print "Consulta inicial de la tabla contactos"
 
-consulta = session.query(model.Contactos).all()
+consulta = session.query(model.Bitacora).all()
 
 for lista in consulta:
 
-    print lista.contacto,lista.numcel
+    print lista.id,lista.mensaje,lista.grupo,lista.numcel,lista.contacto,lista.timestamp,lista.estatus
+    
 
 print "--------------------------------"
 
 
 
-
-
-#Agregar un contacto
-
-contacto = model.Contactos()
-
-contacto.contacto = u"Luisa Gonzalez"
-
-contacto.numcel = u"04155555555"
-print (type(contacto))
-
-session.add(contacto)
-
-session.flush()
-
-session.commit()
-
-
-#Se realiza una consulta a la tabla grupos
-
-print "Consulta la tabla contactos con el dato incorporado"
-
-consulta = session.query(model.Contactos).all()
-
-for lista in consulta:
-
-    print lista.contacto,lista.numcel
-
-print "--------------------------------"
-
-
-
-
-
-
-
-#Borrar un contacto
-#session.delete(contacto)
-
-#session.flush()
-
-#session.commit()
 
 
