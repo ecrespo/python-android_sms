913127b5092974e865e58334dd5cfdad080397d3
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-01 19:53:43 -0430

    Definido property, getter y setter, así como encapsulada la sesión

--- src/bd/bdd.py
+++ src/bd/bdd.py
@@ -17,43 +17,49 @@ def now():
 
 
 
-#Crear un engine y crear todas las tablas necesarias
-
-engine = create_engine('sqlite:///sms.db', echo=False)
-
-model.metadata.bind = engine
-
-model.metadata.create_all()
-
-
-#Se instancia el engine con la base de datos sms.db deshabilitando el eco.
-engine = create_engine('sqlite:///sms.db', echo=False)
-
-#Se define el metadato y se crea
-model.metadata.bind = engine
-model.metadata.create_all()
-
-#Se crea la session
-sm = orm.sessionmaker(bind=engine, autoflush=True, autocommit=False, expire_on_commit=True)
-session = orm.scoped_session(sm)
 
 
 class Crud(object):
 	"""Clase que permite hacer las operaciones basicas en base de datos""" 
-	def __init__(self):
-		pass
+	def __init__(self,base_de_datos):
+		self._db = base_de_datos
+		#Se instancia el engine con la base de datos sms.db deshabilitando el eco.
+		self._engine = create_engine('sqlite:///%s' %self._db,echo=False)
+		#Se define el metadato y se crea
+		model.metadata.bind = self._engine
+		model.metadata.create_all()
+
+		#Se crea la session
+		sm = orm.sessionmaker(bind=self._engine, autoflush=True, autocommit=False, expire_on_commit=True)
+		self._session = orm.scoped_session(self._sm)
+
+	def __getattr__(self,attr):
+		return None
+
+
+	@property
+	def db(self):
+	    return self._db
+	
+	
+    @db.setter
+    def db(self,base_de_datos):
+    	self._db = base_de_datos
 
 	@staticmethod
 	def consulta(tabla):
-		"""Hace consulta en la tabla"""
+		"""Hace consulta en la 
+
+			tabla"""
 		if tabla == "Contactos":
-			consulta = session.query(model.Contactos).all()
-		elif tabla == "Grupos":
-			consulta = session.query(model.Grupos).all()
+			consulta = self._session.query(model.Contactos).all()
+		elif
+		 tabla == "Grupos":
+			consulta = self._session.query(model.Grupos).all()
 		elif tabla == "Mensajes":
-			consulta = session.query(model.Mensajes).all()
+			consulta = self._session.query(model.Mensajes).all()
 		elif tabla == "Bitacora":
-			consulta = session.query(model.Bitacora).all()
+			consulta = self._session.query(model.Bitacora).all()
 
 		return consulta
 
@@ -66,17 +72,17 @@ class Crud(object):
 			contacto.contacto = dato["contacto"]
 			contacto.numcel = dato["numcel"]
 			contacto.grupo = dato["grupo"]			
-			session.add(contacto)
+			self._session.add(contacto)
 
 		elif tabla == "Grupos":
 			grupos = model.Grupos()
 			grupos.nombre = dato["nombre"]
-			session.add(grupos)
+			self._session.add(grupos)
 			
 		elif tabla == "Mensajes":
 			mensajes = model.Mensajes()
 			mensajes.texto = dato["texto"]
-			session.add(mensajes)
+			self._session.add(mensajes)
 		elif tabla == "Bitacora":
 			bitacora = model.Bitacora()
 			bitacora.mensaje = dato["mensaje"]
@@ -84,33 +90,33 @@ class Crud(object):
 			bitacora.numcel = dato["numcel"]
 			bitacora.timestamp = dato["timestamp"]
 			bitacora.estatus = dato["estatus"]
-			session.add(bitacora)
-		session.flush
-		session.commit()
+			self._session.add(bitacora)
+		self._session.flush
+		self._session.commit()
 
     @staticmethod
     def borrar_grupo_contactos(grupo):
     	"""Borra un grupo de contactos de la tabla grupo"""
-        consulta = session.query(model.Contactos).filter(model.Contactos.grupo == grupo).all()
+        consulta = self._session.query(model.Contactos).filter(model.Contactos.grupo == grupo).all()
         for cons in consulta:
-        	session.delete(cons)
-        	session.flush()
-        	session.commit()
+        	self._session.delete(cons)
+        	self._session.flush()
+        	self._session.commit()
 
 	@staticmethod
 	def borrar(tabla,dato):
 		"""Borra un dato de una tabla"""
 		if tabla == "Contactos":
-			consulta = session.query(model.Contactos).filter(model.Contactos.contacto == dato["contacto"]).one()
+			consulta = self._session.query(model.Contactos).filter(model.Contactos.contacto == dato["contacto"]).one()
 		elif tabla == "Grupos":
-			consulta = session.query(model.Grupos).filter(model.Grupos.nombre == dato["nombre"]).one()
+			consulta = self._session.query(model.Grupos).filter(model.Grupos.nombre == dato["nombre"]).one()
 		elif tabla == "Mensajes":
-			consulta = session.query(model.Mensajes).filter(model.Mensajes.texto == dato["texto"]).one()
+			consulta = self._session.query(model.Mensajes).filter(model.Mensajes.texto == dato["texto"]).one()
 		elif tabla == "Bitacora":
-			consulta = session.query(model.Bitacora).filter(model.Bitacora.numcel == dato["numcel"]).one()
-		session.delete(consulta)
-		session.flush()
-		session.commit()
+			consulta = self._session.query(model.Bitacora).filter(model.Bitacora.numcel == dato["numcel"]).one()
+		self._session.delete(consulta)
+		self._session.flush()
+		self._session.commit()
 
 if __name__ == "__main__":
 	crud = Crud()
