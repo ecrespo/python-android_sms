705eca3cc55d2d6b05d3bad380920b112840f667
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-02 14:33:39 -0430

    Ajustada la clase al nuevo estilo e información del módulo

--- src/validar_num.py
+++ src/validar_num.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"Validar números de telefonos"
+
+"""
+Nombre: validar_num.py
+Descripcion: Validar números de telefonos celulares de Venezuela.
+Version: 0.2
+Licencia: GPLv3
+Copyright: Cenditel - Ernesto Crespo <ecrespo@gmail.com>
+Autor: Ernesto Nadir Crespo Avila 
+email: ecrespo@gmail.com
+
+"""
 
 import re, sys 
 
-class validar:
+class validar(object):
 	def __init__(self):
 		pass
 
 
 	def NumCel(self,numero):
-		 if len(numero) == 11 and ((re.search("041[2|4|6]\d\d\d\d\d\d\d",numero)) or (re.search("042[4|6]\d\d\d\d\d\d\d",numero))) :
+		 if len(numero) == 11 and \
+                ((re.search("041[2|4|6]\d\d\d\d\d\d\d",numero)) or \
+                    (re.search("042[4|6]\d\d\d\d\d\d\d",numero))) :
             return True
         else:
             return False
