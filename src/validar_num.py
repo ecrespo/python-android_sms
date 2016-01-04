b56743ab7cb188b66af91ac3cdf086186fb55e33
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-03 23:29:56 -0430

    intento de agregar property, pero lo dejé como estaba, no lo necesita

--- src/validar_num.py
+++ src/validar_num.py
@@ -29,7 +29,7 @@ class Validar(object):
     def __getattr__(self):
         """__getattr__ devuelve None si se trata de acceder a un atributo que no existe"""
         return None
-        
+    
 
     @staticmethod
     def num_cel(num):
