53167b50eb3e6ea2e350f13808466d06264e1357
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-05 16:52:26 -0430

    Arreglada la lógica de detección de dispositivos

--- src/deviceCell.py
+++ src/deviceCell.py
@@ -15,10 +15,10 @@ Email: ecrespo@gmail.com
 
 #import config
 from os import path,environ
-from privilegios import Privilegios
-from commands import getstatusoutput
-
-
+from privilegios import Privilegio
+from SOAPpy import SOAPServer
+import sys 
+from string import find 
 class Cell(object):
     def __init__(self):
         """
@@ -28,7 +28,7 @@ class Cell(object):
         
         self._estado = False
         self._usuario = environ["USERNAME"]
-        self._privilegio = Privilegios(self._usuario) 
+        self._privilegio = Privilegio(self._usuario) 
         
     def __getattr__(self):
         """Devuelve None de atributos que no existen"""
@@ -43,37 +43,92 @@ class Cell(object):
     def usuario(self,usuario):
         """se asigna un nuevo usuario"""
         self._usuario = usuario
-        self._privilegio = Privilegios(self._usuario)
+        self._privilegio = Privilegio(self._usuario)
 
 
     def detectar_dispositivos(self):
         """Detecta los dispositivos android conectados al computador por medio de adb"""
-        resultados = self._privilegio.ejecutar("adb devices")
-        self._lista_dispositivos = []
-        if len(resultados) == 2:
+        resultados = self._privilegio.ejecutar_comando("adb devices")
+        lista_dispositivos = []
+        if len(resultados[1].split("\n")) == 2:
             self._estado = False
             return {"estado":self._estado,"elementos":[]}
-        elif len(resultados) > 2:
-            for dispositivos in resultados[1:-1]:
-                dispositivo = dispositivos[:-1].split("\t")[0]
-                estado_dispositivo = dispositivos[:-1].split("\t")[1]
-                if estado_dispositivo == u'device':
-                    estado_dispositivo = u'activo'
+        elif len(resultados[1].split("\n")) > 2:
+            for dispositivos in resultados[1].split("\n")[:-1]:
+                if (find(dispositivos,"List")) <> -1:
+                    pass
                 else:
-                    estado_dispositivo = u'inactivo'
-                self._lista_dispositivos.append({"dispositivo": dispositivo,"estado": estado_dispositivo})
+                    linea = dispositivos.split("\t")
+                    estado_dispositivo = linea[-1]
+                    dispositivo = linea[0]
+                    if estado_dispositivo == u'offline':
+                        estado = u"inactivo"
+                    elif estado_dispositivo == u'device':
+                        estado = u"activo"
+                    else: 
+                        estado = u"inactivo"
+                    lista_dispositivos.append({"dispositivo":dispositivo,"estado":estado})
             self._estado = True
-            return {"estado":self._estado,"elementos":self.__lista_dispositivos}
+            return {"estado":self._estado,"elementos":lista_dispositivos}
         else:
             self._estado = False
             return {"estado":self._estado,"elementos":[]}
 
+
+
+
+
+class Servicio(object):
+    """Servicio soap para publicar si hay dispositivos o no conectados"""
+    def __init__(self,host="localhost",port=8580):
+        self._host = host
+        self._port = port
+        self._cell = Cell()
+        self._server = SOAPServer((self._host, self._port))
+        self._server.registerFunction(self._cell.detectar_dispositivos)
+
+    def __getattr__(self):
+        """devuelve none si se intenta acceder a un atributo que no existe"""
+        return None
+
+
+    @property
+    def host(self):
+        """getter del host"""
+        return self._host
     
+    @host.setter
+    def host(self,host="localhost"):
+        """setter del host"""
+        self._host = host 
+
+    @property
+    def port(self):
+        """getter del port"""
+        return self._port
+    
+    @port.setter
+    def post(self,port):
+        """setter del puerto """
+        self._port = port
+
+    def iniciar(self):
+        """Se inicia el servicio soap"""
+        self._server.serve_forever()
+
+
+
+
+
 
 if __name__ == "__main__":
+    try:
+        servicio = Servicio()
+        servicio.iniciar()
+    except KeyboardInterrupt:
+        print(u"Finalizada la aplicación")
+        sys.exit()
     
-    cel = Cell()
-    print "Deteccion de dispositivo",cel.detectarDispositivos()
-    cel.guardar_dispositivo()
-    cel.leer_dispositivos()
-    
+    #cell = Cell()
+    #cell.detectar_dispositivos()
+    #print("{0}".format(cell.detectar_dispositivos()))
