c725064c23a063efbd8b1796d1edce1a3f594e8d
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-16 21:40:16 -0430

    Modificado la clase para que soporte el webservice

--- src/pyMensajeSend.py
+++ src/pyMensajeSend.py
@@ -21,15 +21,26 @@ import socket
 
 class sms(object):
     """Clase que permite el envio de sms por conexion USB a celular Android"""
-    def __init__(self):
+    def __init__(self,port):
         """Asignacion de valores a los atributos de la clase"""
-        #self._port = port
-        #self._server = SOAPpy.SOAPProxy("http://localhost:8580/")
+        self._port = port
+        self._server = SOAPpy.SOAPProxy("http://localhost:8580/")
+
         self._validar = Validar()
 
     def __getattr__(self):
         return None
     
+
+    def _ajustar_conf(self):
+        """Permite ajustar la configuracion por medio de adb"""
+        resultado = self._server.detectar_dispositivos()
+        if resultado["estado"] == False: return {"estado":False}
+        self._server.remover_forwarding()
+        resultado = self._server.agregar_forwarding(self._port)
+        return {"estado":resultado["estado"]}
+
+
     def info_cel(self):
         """
         Se verifica si el dispositivo existe.
@@ -37,7 +48,7 @@ class sms(object):
         """
         resultado = self._server.detectar_dispositivos()
         if resultado["estado"] == True:
-            return {"dispositivos":resultado["elementos"][0]["dispositivo"],"estado":resultado["elementos"][0]["estado"]}
+            return resultado["dispositivo"]
         else:
             return {"dispositivos":[],"estado":False}
 
@@ -51,9 +62,8 @@ class sms(object):
         if self._validar.num_cel(numero) == False:
             print(u"Numero no valido")
             return {"estado": False}
-        #Se crea la instancia del objeto Android dependiendo si es conexion
-        #wifi se le pasa el host y el puerto
-        #Si es conexion USB simplemente se crea la instancia
+        resultado = self._ajustar_conf()
+        if resultado["estado"] == False: return {"estado": False}
         droid = android.Android()
         #Enviando el mensaje de texto
         droid.smsSend(numero,mensaje)
@@ -64,15 +74,16 @@ class sms(object):
     
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 3:
-        numero = sys.argv[1]
-        mensaje = sys.argv[2]
+    if len(sys.argv) == 4:
+        port = sys.argv[1]
+        numero = sys.argv[2]
+        mensaje = sys.argv[3]
     else:
-        print ("error enviando mensaje, se necesita pasar el numero y mensaje")
+        print ("error enviando mensaje, se necesita pasar el puerto, numero y mensaje")
         sys.exit
-    andr = sms()
+    andr = sms(port)
     #print(andr.info_cel())
-    resultado = andr.sms_send("04265746477","Epale Dhionel, es ernesto, avisa si llega es otro script 1")
+    resultado = andr.sms_send(numero,mensaje)
     if resultado["estado"] == False:
         print("No se pudo configurar el ambiente para el envio")
     else:
