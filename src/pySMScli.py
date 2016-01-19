66d1d28666403ba2d6b581ccbbe1e6d5ca124799
Ernesto Crespo Avila <ecrespo@gmail.com>
2016-01-18 22:17:18 -0430

    Creado parser de argumentos

--- src/pySMScli.py
+++ src/pySMScli.py
@@ -10,5 +10,26 @@ Email: ecrespo@gmail.com
 version: 0.1
 """
 
-import argparse
+
+
+class app_cli(object):
+    """Clase que genera la interaccion con el servidor de SMS"""
+
+    def __init__(self):
+        """COnstrucctor"""
+        pass
+
+
+
+
+if __name__ == "__main__":
+    import argparse
+    appcli = app_cli()
+    parser = argparse.ArgumentParser(description=u'Procesa envío de sms')
+    parser.add_argument('--multi', type=str, help=u'Envío de sms a multiples números celulares')
+    parser.add_argument('--single', type=int,help=u'Envío de sms a un número celular')
+    parser.add_argument('--info', help=u'Consulta información del celular')
+    args = parser.parse_args()
+    print(args.accumulate(args.integers))
+
 
